5051e8b99177b1636e17358f5b32ddc533d28559
mo3www <linamy3893451@gmail.com>
2025-10-22 02:28:11 +0000

    app.py

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ st.write(f"你選的軟體是: {option}")
 st.write(f"你選的年份是: {year}")
 # 按鈕 (Button)
 if st.button("點我顯示氣球!"):
-st.balloons()
+ st.balloons()
 # 檔案上傳 (File Uploader)
 uploaded_file = st.file_uploader(
 "上傳你的Shapefile (.zip) 或 GeoTIFF (.tif) 或 GeoJSON (.json)",
 type=["zip", "tif", "json"]
 )
 if uploaded_file is not None:
-st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
+ st.success(f"你上傳了: {uploaded_file.name} (大小: {uploaded_file.size} bytes)")
